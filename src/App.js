b4c2a8b6fb6b4728d662f5707e82651fe522ebe0
Luisa <lhurtado372@gmail.com>
2021-05-20 19:31:57 -0500

    reto tareas superado

--- src/App.js
+++ src/App.js
@@ -3,24 +3,46 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  state={
+    string: '',
+    task: ['Sacar la ropa', 'Hacer la cama', 'Leer un rato']
+  }
+   
+  handleChange(e) {
+    this.setState({
+      string: e.target.value
+    })
+  } 
+
+  onSubmit(e) {
+    this.setState({
+      task: [...this.state.task, this.state.string],
+      string: ''
+    })
+    e.preventDefault()
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.task.map((e,i) => <li key={i}>{e}</li>)}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.onSubmit.bind(this)}>
+            <input
+              type="text"
+              id="new-task"
+              placeholder="Ingresa una tarea y oprime Enter"
+              value={this.state.string}
+              onChange={this.handleChange.bind(this)}
+            />
+             
            </form>
         </div>
       </div>
     )
   }
 }
-
-
 export default App;
